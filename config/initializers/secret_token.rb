013eb763210bd21338f4e43fa3addf89c6af0163
Kumar Ishan <kumar.ishan.opensource@gmail.com>
2012-01-13 07:44:30 +0530

    re-doing it

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Echov2::Application.config.secret_token = '96fcdc744bc763628828c28cb3e362a85922462ea61fb6cd644f61aced0107b6d5575d467d647d6c220046150a6b7033f29af8e7fc3c723746901e4c8bfebe93'
