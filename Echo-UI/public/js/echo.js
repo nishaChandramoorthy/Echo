78215735954cf4b0d23e3ea0e2f757c8f59cab99
Kumar Ishan <kumar.ishan.opensource@gmail.com>
2012-02-09 09:20:28 +0530

    daily sync

---
+++ Echo-UI/public/js/echo.js
@@ -0,0 +1,22 @@
+$(function(){
+ 
+  window.MainContentView = Backbone.View.extend({
+    render
+    
+  });
+
+  window.SideBar = Backbone.View.extend({
+    id: "sidebar",
+   
+    events: {
+      "click 
+    },
+ 
+    render: function(){
+
+    }
+
+  });
+
+});
+
