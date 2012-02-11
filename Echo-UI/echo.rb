62678a15f0ae6022581402bc0b0803fb57eea98f
Kumar Ishan <kumar.ishan.opensource@gmail.com>
2012-02-11 16:31:00 +0530

    activity mockup

--- Echo-UI/echo.rb
+++ Echo-UI/echo.rb
@@ -5,3 +5,36 @@ get '/' do
   haml :index
 end
 
+get '/bookshelves' do
+  haml :bookshelves
+end
+
+get '/activity' do
+  haml :activity
+end
+
+get '/search' do 
+  haml :search
+end
+
+get '/suggestions' do
+  haml :suggestions
+end
+
+get '/reading-list' do
+  haml :readingList
+end
+
+get '/communities' do
+  haml :communities 
+end
+
+get '/friends' do
+  haml :friends
+end
+
+get '/history' do
+  haml :history
+end
+
+
--- Echo-UI/public/js/echo.js
+++ Echo-UI/public/js/echo.js
@@ -1,22 +1,80 @@
 $(function(){
- 
-  window.MainContentView = Backbone.View.extend({
-    render
-    
+
+  $("#sb-activity").click(function(){
+    $.ajax({
+      url: "/activity",
+      success: function(data){
+        $("#main-content").html(data);
+        var height = $("#main-content").height();
+        $("#container").height(height);
+      }
+    });
   });
 
-  window.SideBar = Backbone.View.extend({
-    id: "sidebar",
-   
-    events: {
-      "click 
-    },
- 
-    render: function(){
+  $("#sb-search").click(function(){
+    $.ajax({
+      url: "/search",
+      success: function(data){
+        $("#main-content").html(data);
+      }
+    });
+  });
 
-    }
+  $("#sb-suggestions").click(function(){
+    $.ajax({
+      url: "/suggestions",
+      success: function(data){
+        $("#main-content").html(data);
+      }
+    });
 
   });
+
+  $("#sb-bookshelf").click(function(){
+    $.ajax({
+      url: "/bookshelves",
+      success: function(data){
+        $("#main-content").html(data);
+      }
+    });
+  });
+
+  $("#sb-readinglist").click(function(){
+     $.ajax({
+      url: "/reading-list",
+      success: function(data){
+        $("#main-content").html(data);
+      }
+    });
+  });
+
+  $("#sb-communities").click(function(){
+    $.ajax({
+      url: "/communities",
+      success: function(data){
+        $("#main-content").html(data);
+      }
+    });
+  });
+
+  $("#sb-friends").click(function(){
+    $.ajax({
+      url: "/friends",
+      success: function(data){
+        $("#main-content").html(data);
+      }
+    });
+  });
+
+  $("#sb-history").click(function(){
+    $.ajax({
+      url: "/history",
+      success: function(data){
+        $("#main-content").html(data);
+      }
+    });
+  });
+
 
 });
 
