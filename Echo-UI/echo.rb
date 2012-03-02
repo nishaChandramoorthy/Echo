6e1ddbb2e011fb7697bcad2d4c0eff3de73b5952
Abhay Rana <capt.n3m0@gmail.com>
2012-03-02 06:00:12 +0530

    Simpler javascript ajax using load Easier route handling using a single route - The new URLs use /view/:pagename - The url is found for each of the links as per data-url attribute of the link

--- Echo-UI/echo.rb
+++ Echo-UI/echo.rb
@@ -7,40 +7,6 @@ get '/' do
   haml :index2
 end
 
-get '/bookshelves' do
-  haml :bookshelves2
+get '/view/:page' do
+  haml params[:page].to_sym
 end
-
-get '/bookshelf' do
-  haml :bookshelf
-end
-
-get '/activity' do
-  haml :activity
-end
-
-get '/search' do 
-  haml :search2
-end
-
-get '/suggestions' do
-  haml :suggestions
-end
-
-get '/reading-list' do
-  haml :readingList
-end
-
-get '/communities' do
-  haml :communities 
-end
-
-get '/friends' do
-  haml :friends
-end
-
-get '/history' do
-  haml :history
-end
-
-
--- Echo-UI/public/js/echo.js
+++ Echo-UI/public/js/echo.js
@@ -10,86 +10,9 @@ $(function(){
     });
   });
 
-  $("#sb-activity").click(function(){
-    $.ajax({
-      url: "/activity",
-      success: function(data){
-        $("#main-content").html(data);
-        var height = $("#main-content").height();
-        var activity = $('#activity');
-        $activity.imagesLoaded(function(){
-          $activity.masonry({
-            columnWidth: 240
-          });
-        });
-      }
-    });
-  });
-
-  $("#sb-search").click(function(){
-    $.ajax({
-      url: "/search",
-      success: function(data){
-        $("#main-content").html(data);
-        var height = $("#main-content").height();
-        //$("#container").css("height", height);
-      }
-    });
-  });
-
-  $("#sb-suggestions").click(function(){
-    $.ajax({
-      url: "/suggestions",
-      success: function(data){
-        $("#main-content").html(data);
-      }
-    });
-
-  });
-
-  $("#sb-bookshelf").click(function(){
-    $.ajax({
-      url: "/bookshelves",
-      success: function(data){
-        $("#main-content").html(data);
-      }
-    });
-  });
-
-  $("#sb-readinglist").click(function(){
-     $.ajax({
-      url: "/reading-list",
-      success: function(data){
-        $("#main-content").html(data);
-      }
-    });
-  });
-
-  $("#sb-communities").click(function(){
-    $.ajax({
-      url: "/communities",
-      success: function(data){
-        $("#main-content").html(data);
-      }
-    });
-  });
-
-  $("#sb-friends").click(function(){
-    $.ajax({
-      url: "/friends",
-      success: function(data){
-        $("#main-content").html(data);
-      }
-    });
-  });
-
-  $("#sb-history").click(function(){
-    $.ajax({
-      url: "/history",
-      success: function(data){
-        $("#main-content").html(data);
-      }
-    });
+  $('#sidebar>li>a').click(function(){
+	 $('#main-content').load("/view/"+this.getAttribute('data-url'));
+	 return false;//Stops page from scrolling to top
   });
   
   
